415cf4933f61a95895b36108c8527eb8a60b0df6
Jhin <1578979605@qq.com>
2025-04-12 15:54:50 +0800

    新增readme与requirement.txt

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 from views.app_view import create_main_window
 import sys
-sys.path.append(os.path.abspath("./"))  # or os.path.abspath("D:/Project/platform2DMOEAs")
+sys.path.append(os.path.abspath("./"))
 
 
 if __name__ == "__main__":
