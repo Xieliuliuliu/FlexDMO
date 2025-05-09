a133d6ca5abd9d493fbdec757b4c219dea00fb69
hyh <hyhhyh@whu.edu.cn>
2025-05-09 18:42:08 +0800

    实现DIP

--- algorithms/response_strategy/DIP/main.py
+++ algorithms/response_strategy/DIP/main.py
@@ -1,29 +1,102 @@
-import numpy as np
+import math
 
+import numpy as np
+from algorithms.response_strategy.DIP import DIP_ANN
 from algorithms.response_strategy.ResponseStrategy import ResponseStrategy
 from components.Population import Population
-from views.common.GlobalVar import global_vars
+from utils.evolution_tools import getNonDominate
 
 class DIP(ResponseStrategy):
     def __init__(self):
         super().__init__()
 
     def response(self,population, problem, algorithm):
-        population.update_objective_constrain(problem)
-        # PS1, PS2 = None, None
+        X_Low = problem.xl
+        X_Upp = problem.xu
+        DIM = problem.decision_num
+        N = problem.solution_num
         runtime_populations = algorithm.history["runtime"]
-        print(runtime_populations)
-        # if len(runtime_populations.keys()) < 2:
-        #     # 重新初始化
-        #     pop = Population(xl=problem.xl, xu=problem.xu, n_init=problem.solution_num)
-        #     pop.update_objective_constrain(problem)
-        # else:
-        #     # 获取runtime_populations的倒数二个键
-        #     second_last_key, last_key = list(runtime_populations.keys())[-2:]
-        #     second_last_value, last_value = runtime_populations[second_last_key, last_key]
-        #
-        #     last_key_in_second_last_value = list(second_last_value.keys())[-2]
-        #     PS2 = second_last_value[last_key_in_second_last_value]["decision"]
-        #     last_key_in_second_last_value = list(second_last_value.keys())[-1]
-        #     PS1 = second_last_value[last_key_in_second_last_value]["decision"]
+        if len(runtime_populations.keys()) < 2:
+            # 重新初始化
+            population = Population(xl=problem.xl, xu=problem.xu, n_init=problem.solution_num)
+            population.update_objective_constrain(problem)
+        else:
+            # 获取runtime_populations的倒数二个键(最后两个t)
+            t_2, t_1 = list(runtime_populations.keys())[-2:]
+            # 获取最后两个环境(t)的最后一次进化结果作为历史PS
+            env_2= runtime_populations[t_2]
+            env_1 = runtime_populations[t_1]
+            env_2_last_eval = list(env_2.keys())[-1]
+            PS2 = env_2[env_2_last_eval].get_decision_matrix()
+            env_1_last_eval = list(env_1.keys())[-1]
+            PS1 = env_1[env_1_last_eval].get_decision_matrix()
+            pop1 = (Population(xl=X_Low, xu=X_Upp, X=PS1))
+            pop1.update_objective_constrain(problem)
+            pop2 = Population(xl=X_Low, xu=X_Upp, X=PS2)
+            pop2.update_objective_constrain(problem)
+
+            ann = DIP_ANN.ANN(X_Low, X_Upp, DIM, 5)
+            input, target = get_input_target(pop2, pop1, N)
+            DIP_ANN.train(ann, input, target, X_Low, X_Upp)
+            population = DIP_init_pop(input, target, PS1, PS2, ann, X_Low, X_Upp, N)
+            population.update_objective_constrain(problem)
         return population
+
+def get_input_target(pop2, pop1, N):
+    non_pop1 = getNonDominate(pop1)
+    non_pop2 = getNonDominate(pop2)
+    x1 = non_pop1.get_decision_matrix()
+    x2 = non_pop2.get_decision_matrix()
+    ns = min(len(x1), len(x2))
+
+    x1 = x1[:ns,:]
+    x2 = x2[:ns,:]
+    #数量不够，要增加数据
+    if ns < math.ceil(N / 2):
+        need = math.ceil(N / 2) - ns
+        std = np.std(x2, axis=0)
+        indexs = np.random.choice(ns, need, replace=True)
+        x1_add = x1[indexs]
+        x2_add = x2[indexs]
+        # 对采样的 x2 加噪声
+        noise = np.random.normal(0, std, size=(need, x2.shape[1]))
+        x2_add += noise
+
+        # 合并得到最终的训练集
+        input = np.vstack([x2, x2_add])
+        target = np.vstack([x1, x1_add])
+    else:
+        input = x2
+        target = x1
+    return input, target
+
+def DIP_init_pop(input, target, PS1, PS2, ann, X_Low, X_Upp, N):
+    xl = DIP_ANN.predict_by_ann(ann, target, X_Low, X_Upp)
+    ns = xl.shape[0]
+    need = N - ns
+    random_factors1 = np.random.random(size=ns)[:, np.newaxis]
+    random_factors2 = np.random.random(size=ns)[:, np.newaxis]
+    part1 = random_factors1 * (xl - target)
+    part2 = random_factors2 * (target - input)
+    xp = target + part1 + part2
+
+    #随机选取x_fill直到满足数量需要
+    x_fill = DIP_ANN.predict_by_ann(ann, PS1, X_Low, X_Upp)
+    rand_arr = np.random.choice(np.arange(len(x_fill)), size=need, replace=True)
+    x_add = x_fill[rand_arr]
+    #合并
+    new_xp = np.vstack([xp, x_add])
+
+    # 将 new_xp 中的解进行还原
+    new_xp_reversed = DIP_ANN.inverse_data(new_xp, X_Low, X_Upp)
+
+    # 边界处理
+    for i in range(N):
+        for j in range(xl.shape[1]):
+            if new_xp_reversed[i, j] < X_Low[j]:
+                new_xp_reversed[i, j] = 0.5 * (PS2[i, j] + X_Low[j])
+            if new_xp_reversed[i, j] > X_Upp[j]:
+                new_xp_reversed[i, j] = 0.5 * (PS2[i, j] + X_Upp[j])
+
+    population = Population(xl=X_Low, xu=X_Upp, X = new_xp_reversed)
+    return population
